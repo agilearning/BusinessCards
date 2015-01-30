28e5d9884ce85acf46d81f5b6c82ed9893eb2e99
adrianliaw <adrianliaw2000@gmail.com>
2015-01-30 16:40:48 +0800

    hsiang

---
+++ hsiang.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import wudukers
+
+
+@wudukers.intern
+def hsiang(**my):
+
+    assert my["name"] is "Wen-Hsiang Shih"
+    assert my["github"] is "http://github.com/wen777"
+    assert my["email"] is "shih777577@gmail.com"
+    assert my["phone_number"] is "+886 912-900-689"
+    assert wudukers.VAT_ID is "24755908"
+
+
