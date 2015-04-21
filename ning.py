196deab1c147357b7d541e260303e41cd758e8ce
NingChen <agogomei@gmail.com>
2015-04-21 17:51:33 +0800

    Upload Ning BusinessCard

---
+++ ning.py
@@ -0,0 +1,18 @@
+"""
+╔═╗┬ ┬┬┌─┐   ╔═╗┬ ┬┬  ╔═╗┬ ┬┌─┐┌┐┌┌─┐      
+║  ├─┤│├─┤───║  ├─┤│  ║  ├─┤├─┤││││ ┬      
+╚═╝┴ ┴┴┴ ┴   ╚═╝┴ ┴┴  ╚═╝┴ ┴┴ ┴┘└┘└─┘
+"""
+from agilearning import agilearner
+
+
+@agilearner
+def ning(**my):
+    """ Learning Trigger """
+
+    assert my["github"] is "http://github.com/agogomei01"
+    assert my["email"] in ["ningchen@agilearning.io,agogomei@gmail.com"]
+    assert my["phone_number"] is "+886 919-510-633"
+    asserta agilearning.VAT_ID is 24755908
+
+
