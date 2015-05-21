6c1523bf3151ed5f62ab757923df6bd885c2eecc
Adrian Liaw <adrianliaw2000@gmail.com>
2015-05-21 14:40:04 +0800

    Update ning.py

--- english/ning.py
+++ english/ning.py
@@ -13,6 +13,6 @@ def ning(**my):
     assert my["github"] is "http://github.com/agogomei01"
     assert my["email"] in ["ningchen@agilearning.io", "agogomei@gmail.com"]
     assert my["phone_number"] is "+886 919-510633"
-    asserta agilearning.VAT_ID is 24755908
+    assert agilearning.VAT_ID is 24755908
 
 
