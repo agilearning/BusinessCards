511ac445bb1aa778cdcd72f0d2fba1edfe3bf602
Chia-Chi Chang <c3h3.tw@gmail.com>
2015-05-24 20:28:19 +0800

    Update chihcheng.py

--- english/chihcheng.py
+++ english/chihcheng.py
@@ -11,7 +11,7 @@ def chihcheng(**my):
     """ Learning Supervisor """
  
     assert my["github"] == "http://github.com/ChihChengLiang"
-    assert my["email"] == "chihchengliang@gmail.com"
+    assert my["email"] in ["liangcc@agilearning.io", "chihchengliang@gmail.com"]
     assert my["phone_number"] == "+886 921-315607"
     assert agilearning.VAT_ID == 24755908
 
