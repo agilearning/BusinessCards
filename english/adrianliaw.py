4710da00d967dd0830da87a7011787be505fbdb2
Chia-Chi Chang <c3h3.tw@gmail.com>
2015-05-24 20:06:23 +0800

    Update adrianliaw.py

--- english/adrianliaw.py
+++ english/adrianliaw.py
@@ -11,7 +11,7 @@ def adrianliaw(**my):
     """ Learning Reinforcer """
 
     assert my["github"] == "http://github.com/adrianliaw"
-    assert my["email"] == "adrianliaw2000@gmail.com"
+    assert my["email"] in ["whliaw@agilearning.io", "adrianliaw2000@gmail.com"]
     assert my["phone_number"] == "+886 976-407095"
     assert agilearning.VAT_ID == 24755908
 
