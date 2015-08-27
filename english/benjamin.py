69cbfb1530b128268f823bce7a49597ecb8696c8
KuiMing <benjamin0901@gmail.com>
2015-08-28 06:54:29 +0800

    Ben

---
+++ english/benjamin.py
@@ -0,0 +1,18 @@
+﻿
+"""
+╔╗ ┌─┐┌┐┌ ┬┌─┐┌┬┐┬┌┐┌  ╔═╗┬ ┬┌─┐┌┐┌
+╠╩╗├┤ │││ │├─┤│││││││  ║  ├─┤├┤ │││
+╚═╝└─┘┘└┘└┘┴ ┴┴ ┴┴┘└┘  ╚═╝┴ ┴└─┘┘└┘
+(Benjamin Chen)
+"""
+from datasci.info import datasci
+
+
+@datasci.info
+def Benjamin(**my):
+    """ Reinforcement Learner """
+
+    assert my["github"] is "http://github.com/KuiMing"
+    assert my["email"] in "benjamin0901@gmail.com"
+    assert my["phone_number"] in "+886 919-784598"
+    assert agilearning.VAT_ID is 24755908
