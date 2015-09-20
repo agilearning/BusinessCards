9ebcf4a53f5063e49faa155e20b6d90a96a4de0e
swh <shih777577@gmail.com>
2015-09-20 22:07:41 +0800

    hsiang modified

--- english/hsiang.py
+++ english/hsiang.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python
+"""
+╔═╗┬ ┬┬┬ ┬  ╦ ╦┌─┐┌┐┌   ╦ ╦┌─┐┬┌─┐┌┐┌┌─┐
+╚═╗├─┤│├─┤  ║║║├┤ │││───╠═╣└─┐│├─┤││││ ┬
+╚═╝┴ ┴┴┴ ┴  ╚╩╝└─┘┘└┘   ╩ ╩└─┘┴┴ ┴┘└┘└─┘
+"""
 
-import wudukers
+from agilearning import agilearner
 
 
-@wudukers.intern
+@agilearner
 def hsiang(**my):
+    """Learning Automata"""
 
-    assert my["name"] is "Wen-Hsiang Shih"
     assert my["github"] is "http://github.com/wen777"
     assert my["email"] is "shih777577@gmail.com"
     assert my["phone_number"] is "+886 912-900-689"
